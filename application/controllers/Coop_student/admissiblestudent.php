d5c484b185db20afe07732998e1e8a8357f65d9d
santikon <san5tikon12@gmail.com>
2018-01-13 17:04:39 +0700

    admissiblestudent

---
+++ application/controllers/Coop_student/admissiblestudent.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class admissiblestudent  extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        if(!$this->Login_session->check_login()) {
+            $this->session->sess_destroy();
+            redirect('member/login');
+		}
+		
+		//check priv
+        if($this->Login_session->check_login()->login_type != 'coop_student') {
+            redirect($this->Login_session->check_login()->login_type);
+            die();
+        }
+    }
+
+    public function index()
+	{
+        
+        $data['data'] = $this->Coop_student_Adimssiblestudent->get_adimssiblestudent(9);
+        $this->template->view('Coop_student/admissiblestudent_view',$data);
+       
+    }
+
+    public function from_adimssiblestudent(){
+        $data['data'] = $this->Coop_student_Adimssiblestudent->get_adimssiblestudent();
+         print_r($data);
+    }
+
+}
---
+++ application/models/Coop_student/Coop_student_info_model.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Coop_student_info_model extends CI_Model {
+   /* public function get(){
+        $this->db->select('id')
+        $this->db->from('student');
+        $query = $this->db->get();
+        return $query->result();
+    }*/
+}
