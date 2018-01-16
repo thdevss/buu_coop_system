b5892fc3c8e2c5af95a45ed9631147e8404001af
thdevss <iam@devss.pw>
2018-01-16 13:31:59 +0700

    ldap service

--- application/models/BUUMember_model.php
+++ application/models/BUUMember_model.php
@@ -1,26 +1,42 @@
 <?php
 class BUUMember_model extends CI_Model 
 {
-    public function login($username, $password)
+    public function xlogin($username, $password)
     {
-        if($username == '57660132') {
-            $data = array();
-            $data['fullname'] = 'Pichet S.';
-            $data['login_type'] = 'student';
-        } else if($username == 'nuttanon@buu.ac.th') {
+        if($username == 'nuttanon@buu.ac.th') {
             $data = array();
             $data['fullname'] = 'Nuttanon';
             $data['login_type'] = 'teacher';
-        } else if($username == '57660135') {
-            $data = array();
-            $data['fullname'] = 'Santikon A.';
-            $data['login_type'] = 'coop_student';
         } else if($username == 'pnut') {
             $data = array();
             $data['fullname'] = 'Kamonwan';
             $data['login_type'] = 'officer';
         }
 
+        return $data;
+    }
+
+    public function login($username, $password)
+    {
+        $this->ldap->connect();
+        if($this->ldap->authenticate('' , $username, $password)) {
+            $userdata = $this->ldap->get_data($username,$password);
+            if($userdata['ou'] == 'students') {
+                //coop student and student
+                $data['fullname'] = $userdata['fname'].' '.$userdata['lname'];                
+                if($this->Coop_student->get($userdata['code'])) {
+                    //coop student
+                    $data['login_type'] = 'coop_student';
+                } else {
+                    //student
+                    $data['login_type'] = 'student';                    
+                }
+            } else {
+                return $this->xlogin($username, $password);
+            }
+        } else {
+            return $this->xlogin($username, $password);
+        }
         return $data;
     }
 
