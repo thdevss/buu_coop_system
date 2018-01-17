08c3f4a46bbc09f1702480bdac755749da7a0dc4
thdevss <iam@devss.pw>
2018-01-17 14:48:15 +0700

    .

---
+++ application/language/english/form_validation_lang.php
@@ -0,0 +1,6 @@
+<?php
+$lang['form_validation_required'] = '{field}, โปรดกรอกข้อมูลในช่อง';
+$lang['form_validation_numeric'] = '{field}, โปรดกรอกเฉพาะตัวเลข';
+$lang['form_validation_valid_email'] = '{field}, โปรดกรอกอีเมลให้ถูกต้อง';
+$lang['form_validation_valid_email'] = '{field}, โปรดกรอกอีเมลให้ถูกต้อง';
+
