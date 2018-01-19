24cfcf17a7ae6551ffbc0478c073c80aa32d15a2
santikon <san5tikon12@gmail.com>
2018-01-19 15:03:13 +0700

    19-jan-18:30:03

--- application/controllers/Officer/Test_Management.php
+++ application/controllers/Officer/Test_Management.php
@@ -2,6 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test_Management extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        if(!$this->Login_session->check_login()) {
+            $this->session->sess_destroy();
+            redirect('member/login');
+		}
+		
+		//check priv
+        if($this->Login_session->check_login()->login_type != 'officer') {
+            redirect($this->Login_session->check_login()->login_type);
+            die();
+        }
+    }
 
     public function index(){
         $data['data'] = array();
