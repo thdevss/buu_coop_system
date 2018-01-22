bfbe2b8b6e2d9f02038017b900e9d0a5f05d71e7
santikon <san5tikon12@gmail.com>
2018-01-22 14:58:49 +0700

    Document

---
+++ application/controllers/Document.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Document extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        if(!$this->Login_session->check_login()) {
+            $this->session->sess_destroy();
+            redirect('member/login');
+		}
+		
+		//check priv
+        if($this->Login_session->check_login()->login_type != 'coop_student') {
+            redirect($this->Login_session->check_login()->login_type);
+            die();
+        }
+    }
+
+    public function index() {
+        $this->load->view('Document/IN-S001_view');
+    }
+
+}
