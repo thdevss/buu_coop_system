66750f88b107e955b2bc3de44b612927f91fde16
thdevss <iam@devss.pw>
2018-01-22 18:05:58 +0700

    pdf_lib

---
+++ application/views/Document/test.php
@@ -0,0 +1,46 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<table width="100%">
+    <tr>
+        <td width="10%">
+            <img src="<?php echo ('assets/img/buu_logo.jpg');?>" width="90" height="90" border="0" align="center" >
+        </td>
+
+        <td width="80%">
+            <center>
+            <h2 style="text-align:center;margin-button:0px;">
+		        ใบสมัครเป็นนิสิตสหกิจศึกษา
+            </h2>
+            <h3>
+                วิชาสหกิจศึกษา คณะวิทยาการสารสนเทศ มหาวิทยาลัยบูรพา
+	        </h3>
+            <h3>
+            Cooperative Education --- Faculty of Informatics Burapha University
+	        </h3>
+            </center>
+        </td>
+
+        <td width="10%" valign="top">
+            <div align="right">IN-S001</div>
+        </td>
+    </tr>
+
+</table>
+<table width="100%">
+    <tr>
+        <td width="25%" align="right">
+            <h3>1.ชื่อ-นามสกุล</h3>
+        </td>
+
+        <td width="70%">
+            <h4 style="text-align:center;margin-button:0px;">
+            (ไทย) นาย/น.ส.
+            </h4>
+        </td>
+
+        <td width="10%" valign="top">
+        <h4 style="text-align:center;margin-button:0px;">
+        </td>
+    </tr>
+
+</table>
