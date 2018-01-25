767f55e69974fe1e509302e5a07e45d3d5179582
vocadew <57160419@go.buu.ac.th>
2018-01-25 15:52:18 +0700

    officer3,4,8-edit

--- application/models/BUUMember_model.php
+++ application/models/BUUMember_model.php
@@ -3,7 +3,7 @@ class BUUMember_model extends CI_Model
 {
     public function xlogin($username, $password)
     {
-        if($username == 'nuttanon@buu.ac.th') {
+        if($username == 'nutthanon9') {
             $data = array();
             $data['fullname'] = 'Nuttanon';
             $data['login_type'] = 'teacher';
@@ -12,7 +12,7 @@ class BUUMember_model extends CI_Model
             $data = array();
             $data['fullname'] = 'Kamonwan';
             $data['login_type'] = 'officer';
-            $data['login_value'] = 'kamonwan';
+            $data['login_value'] = 'kamonwans';
         }
 
         return $data;
