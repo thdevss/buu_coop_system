7f37ba9e089f1e2b65c30377f6f4c54380686e87
thdevss <iam@devss.pw>
2018-03-01 17:26:33 +0700

    rollback .js file

---
+++ assets/js/officer/term.js
@@ -0,0 +1,30 @@
+function changeTermAjax()
+{
+    var term_id = jQuery('#term_option').find(":selected").val()
+
+    changeTerm(term_id)
+}
+
+$('#term_option_menu').change(function() {
+    changeTerm(jQuery(this).val())
+})
+
+
+function changeTerm(term_id)
+{
+    var data = { term_id: term_id }
+    jQuery.post(SITE_URL+"/officer/change_term/ajax_change/", data, function(response) {
+        // Do something with the request
+        $("#selectTermBox").modal('hide')
+        //alert
+        swal({
+            title: "เปลี่ยนปีการศึกษาเรียบร้อย!",
+            text: "ทำ",
+            icon: "success",
+          })
+          .then((xxx) => {
+            window.location.reload();
+          });
+
+    }, 'json');
+}
