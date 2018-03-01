de7b48f3f46051c6e9b42f6410f6198c3d0ac5df
thdevss <iam@devss.pw>
2018-03-01 16:41:50 +0700

    student setting skill

--- application/models/Skill_Search_model.php
+++ application/models/Skill_Search_model.php
@@ -1,12 +1,18 @@
 <?php
 class Skill_Search_model extends CI_model {
+
     public function search_skill_by_student($student_id)
     {
-        $this->db->where('student_id',$student_id);
+        $this->db->where('student_id', $student_id);
         $this->db->from('student_has_skill');
         $query = $this->db->get();
         return $query->result_array();
+    }
 
+    public function delete_by_student($student_id) 
+    {
+        $this->db->where('student_id', $student_id);
+        return $this->db->delete('student_has_skill');
     }
 
     public function search_student_by_skill($skill_id)
