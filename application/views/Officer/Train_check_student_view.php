d38c174c909afad83d6c38035f23e8653fb396cf
Santikon apai <santikon12@gmail.com>
2018-03-12 17:05:46 +0700

    135_good

--- application/views/Officer/Train_check_student_view.php
+++ application/views/Officer/Train_check_student_view.php
@@ -10,7 +10,7 @@
 <div class="container-fluid">
     <div class="animated fadeIn">
         <div class="row" >
-            <div class="col-sm-12">
+            <div class="col-sm-4">
                 <div class="card">
                     <div class="card-header"><i class="fa fa-align-justify"></i> เช็คชื่อเข้าอบรม</div>
                     <div class="card-body">     
@@ -21,7 +21,7 @@
                         ?>        
                         <form action="<?php echo site_url('officer/Train_check_student/check');?>" method="post">
                             <div class="row">
-                                <div class="form-group col-md-6 offset-md-3">
+                                <div class="form-group col-md-12">
                                     <label for="building">โครงการ</label>          
                                     <select class="form-control" name="train_id">
                                         <option>--- please select ----</option>
@@ -31,7 +31,7 @@
                                     </select>
                                 </div>
 
-                                <div class="form-group col-md-6 offset-md-3">
+                                <div class="form-group col-md-12">
                                     <label for="note">บันทึกช่วยจำ</label>          
                                     <input class="form-control" type="text" name="note" id="note">
                                 </div>
@@ -45,7 +45,39 @@
                     </div>
                 </div>
             </div>
+
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"><i class="fa fa-align-justify"></i> รายการเช็คชื่อ</div>
+                    <div class="card-body">
+                        <table class="table table-bordered" id="student_table">
+                            <thead>
+                                <tr>
+                                  <th></th>
+                                  <th>วันที่</th>
+                                  <th>ครั้งที่</th>
+                                  <th>โครงการ</th>
+                                  <th></th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                  <td class="text-center"></td>
+                                  <td class="text-left"></td>
+                                  <td class="text-right"></td>
+                                  <td class="text-left"></td>
+                                  <td class="text-center">
+                                    <?php echo anchor('Officer/Training/#/'.$row['train']['id'], '<i class="fa fa-list"></i> รายชื่อนิสิต', 'class="btn  btn-primary"');?>
+                                  </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </div>
 </div>
 
