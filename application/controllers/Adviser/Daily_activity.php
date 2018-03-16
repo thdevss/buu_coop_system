9087d2400be57ebe804b1b6cd58205e99dbdf0cf
Santikon apai <santikon12@gmail.com>
2018-03-16 14:25:25 +0700

    135_good:2:25

--- application/controllers/Adviser/Daily_activity.php
+++ application/controllers/Adviser/Daily_activity.php
@@ -11,10 +11,14 @@ class Daily_activity extends CI_controller
         }
 
         //check priv
-        if($this->Login_session->check_login()->login_type != 'adviser') {
+        $user = $this->Login_session->check_login();
+        if($user->login_type != 'adviser') {
             redirect($this->Login_session->check_login()->login_type);
             die();
         }
+
+        
+        $this->breadcrumbs->push(strToLevel($user->login_type), '/'.$user->login_type); //actor
     }
 
     public function index()
@@ -29,6 +33,11 @@ class Daily_activity extends CI_controller
             $tmp_array['company_address'] = $this->Address->get_address_by_company($row['company_id'])[0];
             array_push($data['data'], $tmp_array);
         }
+
+        // add breadcrumbs
+        $this->breadcrumbs->push('กิจกรรมในการฝึกงานในแต่ละวัน', '/Adviser/Daily_activity/index');
+        
+
         $this->template->view('Adviser/Daily_activity_lists_student_view',$data);
     }
 
@@ -40,6 +49,11 @@ class Daily_activity extends CI_controller
        }
         $data['student'] = $this->Student->get_student($student_id)[0];
         $data['data'] = $this->Daily_Report->gets_report_by_student($student_id);
+
+        // add breadcrumbs
+        $this->breadcrumbs->push('กิจกรรมในการฝึกงานในแต่ละวัน', '/Adviser/Daily_activity/index');
+        $this->breadcrumbs->push('รายการกิจกรรมฝึกงาน', '/Adviser/Daily_activity/lists/'.$student_id);
+
         $this->template->view('Adviser/Daily_activity_list_view',$data);
     }
 
@@ -50,7 +64,13 @@ class Daily_activity extends CI_controller
         if(!$data['data']){
             show_404();
         }
-       $this->template->view('Adviser/Daily_detail_view',$data);
+
+        // add breadcrumbs
+        $this->breadcrumbs->push('กิจกรรมในการฝึกงานในแต่ละวัน', '/Adviser/Daily_activity/index');
+        $this->breadcrumbs->push('รายการกิจกรรมฝึกงาน', '/Adviser/Daily_activity/lists/'.$data['data']['student_id']);
+        $this->breadcrumbs->push('รายละเอียดกิจกรรมฝึกงาน', '/Adviser/Daily_activity/detail/'.$id);
+
+        $this->template->view('Adviser/Daily_detail_view',$data);
     }
 
 }
--- application/views/Adviser/Daily_activity_lists_student_view.php
+++ application/views/Adviser/Daily_activity_lists_student_view.php
@@ -2,11 +2,9 @@
 <main class="main">
 
 <!-- Breadcrumb -->
-<ol class="breadcrumb">
-  <li class="breadcrumb-item">Home</li>
-  <li class="breadcrumb-item"><a href="#"><?php echo strToLevel($user->login_type);?></a></li>
-  <li class="breadcrumb-item active">กิจกรรมในการฝึกงานในแต่ละวัน</li>
-</ol>
+<?php echo $this->breadcrumbs->show(); ?>
+
+
 <div class="container-fluid">
   <div class="animated fadeIn">
     <div class="row" >
